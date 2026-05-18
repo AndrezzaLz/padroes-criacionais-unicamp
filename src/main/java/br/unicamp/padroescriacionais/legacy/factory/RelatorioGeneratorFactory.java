1ae328dec318ec2b10002b52e350e6b45cad08b3
૮₍ ´• ˕ •` ₎ა <167632296+strawpies@users.noreply.github.com>
2026-05-18 14:33:52 -0300

    Implement RelatorioGeneratorFactory for report generation

---
+++ src/main/java/br/unicamp/padroescriacionais/legacy/factory/RelatorioGeneratorFactory.java
@@ -0,0 +1,33 @@
+package br.unicamp.padroescriacionais.legacy.factory;
+
+import br.unicamp.padroescriacionais.legacy.domain.FormatoRelatorio;
+import br.unicamp.padroescriacionais.legacy.generator.*;
+
+public class RelatorioGeneratorFactory {
+
+    public static RelatorioGenerator criar(FormatoRelatorio formato) {
+
+        switch (formato) {
+
+            case PDF:
+                return new PdfRelatorioGenerator();
+
+            case CSV:
+                return new CsvRelatorioGenerator();
+
+            case JSON:
+                return new JsonRelatorioGenerator();
+
+            case XML:
+                return new XmlRelatorioGenerator();
+
+            case HTML:
+                return new HtmlRelatorioGenerator();
+
+            default:
+                throw new IllegalArgumentException(
+                        "Formato desconhecido: " + formato
+                );
+        }
+    }
+}
